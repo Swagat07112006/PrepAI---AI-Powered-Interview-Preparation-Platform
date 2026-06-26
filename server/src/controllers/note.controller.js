16169ff8b3b61424f5d0164c93f20621108eb2d7
Swagat Kumar Sahoo <swagatkumarsahoo1807@gmail.com>
2026-06-26 09:02:52 +0530

    feat: implement CRUD operations for notes; add list, get, update, and delete functionalities with appropriate authorization checks

--- server/src/controllers/note.controller.js
+++ server/src/controllers/note.controller.js
@@ -25,4 +25,81 @@ const createNote = asyncHandler(async (req, res) => {
     )
 })
 
-export {createNote}
+const listNotes = asyncHandler(async (req, res) => {
+    const userId = req.user._id;
+    const userNotes = await Note.find({ userId: userId}).sort({ createdAt: -1})
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            userNotes,
+            "User Notes fetched successfully"
+        )
+    )
+})
+
+const getNote = asyncHandler(async ( req, res) => {
+    const { id } = req.params;
+    const note = await Note.findById(id)
+    if(!note){
+        throw new ApiError(404, "Note not found");
+    }
+    if(req.user._id.toString() !== note.userId.toString()){
+        throw new ApiError(403, "Request Forbidden")
+    }
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            note,
+            "Note fetched successfully"
+        )
+    )
+})
+
+const updateNote = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const updates = req.body;
+    const note = await Note.findById(id);
+    if(!note){
+        throw new ApiError(404, "Note not found")
+    }
+    if(note.userId.toString() !== req.user._id.toString()){
+        throw new ApiError(403, "Request Forbidden")
+    }
+    const allowedFields = ["title", "topics", "content", "tags"]
+    for(const field in updates){
+        if(allowedFields.includes(field)){
+            note[field] = updates[field]
+        }
+    }
+    await note.save()
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            note,
+            "Note updated successfully"
+        )
+    )
+})
+
+const deleteNote = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    const note = await Note.findById(id);
+    if(!note){
+        throw new ApiError(404, "Note not found")
+    }
+    if(note.userId.toString() !== req.user._id.toString()){
+        throw new ApiError(403, "Request Forbidden")
+    }
+    await note.deleteOne()
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            {},
+            "Note deleted successfully"
+        )
+    )
+})
+
+export {createNote, listNotes, getNote, updateNote, deleteNote}
--- server/src/routes/note.routes.js
+++ server/src/routes/note.routes.js
@@ -1,27 +1,12 @@
 import {Router} from 'express'
-import { createNote } from '../controllers/note.controller';
+import { createNote, deleteNote, getNote, listNotes, updateNote } from '../controllers/note.controller.js';
+import authMiddleware from '../middlewares/auth.middleware.js'
 const router = Router();
 
-router.route('/').post(createNote)
-router.route('/').get((req, res) => {
-    res.status(501).json({
-        msg: "Not Implemented"
-    })
-})
-router.route('/:id').get((req, res) => {
-    res.status(501).json({
-        msg: "Not Implemented"
-    })
-})
-router.route('/:id').put((req, res) => {
-    res.status(501).json({
-        msg: "Not Implemented"
-    })
-})
-router.route('/:id').delete((req, res) => {
-    res.status(501).json({
-        msg: "Not Implemented"
-    })
-})
+router.route('/').post(authMiddleware, createNote)
+router.route('/').get(authMiddleware, listNotes)
+router.route('/:id').get(authMiddleware, getNote)
+router.route('/:id').put(authMiddleware, updateNote)
+router.route('/:id').delete(authMiddleware, deleteNote)
 
 export default router
