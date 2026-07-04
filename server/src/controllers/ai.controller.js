d28822406326bdbfc3cd7e7afe15e500d6b2f3fc
Swagat Kumar Sahoo <swagatkumarsahoo1807@gmail.com>
2026-07-04 10:39:36 +0530

    fix: update generateRoadmap function to use dynamic prompt content

---
+++ server/src/controllers/ai.controller.js
@@ -0,0 +1,30 @@
+import { generateRoadmap } from '../services/ai.service';
+import asyncHandler from '../utils/asyncHandler.js'
+const generateAIRoadmap = asyncHandler(async (req, res) => {
+    const {
+        targetCompany,
+        currentLevel,
+        role,
+        timeAvailable,
+        hoursPerDay,
+        skills,
+    } = req.body;
+
+    const roadmap = await generateRoadmap({
+        targetCompany,
+        currentLevel,
+        role,
+        timeAvailable,
+        hoursPerDay,
+        skills,
+    })
+
+    return res.status(200).json(
+        new ApiResponse(
+            200,
+            roadmap,
+            "AI Roadmap generated successfully"
+        )
+    )
+
+})
--- server/src/services/ai.service.js
+++ server/src/services/ai.service.js
@@ -5,7 +5,7 @@ const generateRoadmap = async (roadmapData) => {
     const prompt = roadmapPrompt(roadmapData)
     const response = await ai.models.generateContent({
         model: 'gemini-2.5-flash',
-        contents: 'Why is the sky blue?',
+        contents: prompt
     });
     return response.text
 }
